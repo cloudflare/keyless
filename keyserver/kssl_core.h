e9bbf07e2e16d01552f35351350cae30b1005dbe
Nick Sullivan <nick@cloudflare.com>
2014-01-07 14:42:49 -0800

    Improve readability of headers.

--- keyserver/kssl_core.h
+++ keyserver/kssl_core.h
@@ -10,18 +10,18 @@
 // Allocate and populate a response to a keyless SSL request
 // using an opaque list of private keys response to be freed by caller
 kssl_error_code kssl_operate(
-    kssl_header *header,
-    BYTE *payload,
-    pk_list privates,
-    BYTE **response,
-    int *response_len);
+    kssl_header *header,        // pointer to the incoming header
+    BYTE        *payload,       // pointer to the incoming payload
+    pk_list      privates,      // reference to list of private keys
+    BYTE       **response,      // response to be freed by caller
+    int         *response_len); // length of response
 
 // Create a keyless SSL response message corresponding to an error
 // response to be freed by caller
 kssl_error_code kssl_error(
-    DWORD id,
-    BYTE error,
-    BYTE **response,
-    int *response_len);
+    DWORD       id,             // id of error to create
+    BYTE        error,          // value of error
+    BYTE      **response,       // response to be freed by caller
+    int        *response_len);  // length of response
 
 #endif // INCLUDED_KSSL_CORE
--- keyserver/kssl_private_key.h
+++ keyserver/kssl_private_key.h
@@ -24,33 +24,38 @@ void free_pk_list(pk_list list);
 // add_key_from_file: adds an RSA key from a file location, returns
 // KSSL_ERROR_NONE if successful, or a KSSL_ERROR_* if a problem
 // occurs. Adds the private key to the list if successful.
-kssl_error_code add_key_from_file(const char *path, // Path to file containing key
-                                  pk_list list);    // Array of private keys from new_pk_list
+kssl_error_code add_key_from_file(
+  const char *path,     // Path to file containing key
+  pk_list     list);    // Array of private keys from new_pk_list
 
 // add_key_from_buffer: adds an RSA key from a pointer, returns
 // KSSL_ERROR_NONE if successful, or a KSSL_ERROR_* if a problem
 // occurs. Adds the private key to the list if successful.
-kssl_error_code add_key_from_buffer(const char *key, // Key value in PEM format
-                                    int key_len,     // Length of key in bytes
-                                    pk_list list);   // Array of private keys 
+kssl_error_code add_key_from_buffer(
+  const char *key,      // Key value in PEM format
+  int         key_len,  // Length of key in bytes
+  pk_list     list);    // Array of private keys 
 
 // find_private_key: returns an id for the key that matches the digest.
 // In this implementation key id is the index into the list of privates.
 // A negative return indicates an error.
-int find_private_key(pk_list list,   // Array of private keys from new_pk_list
-                     BYTE *digest);  // Digest of key searched for (see digest_public_modulus)
+int find_private_key(
+  pk_list     list,     // Array of private keys from new_pk_list
+  BYTE       *digest);  // Digest of key to search for (see digest_public_modulus)
 
 // private_key_operation: perform a private key operation
-kssl_error_code private_key_operation(pk_list list,         // Private key array from new_pk_list
-                                      int key_id,           // ID of key in pk_list from find_private_key
-                                      int opcode,           // Opcode from a KSSL message indicating the operation
-                                      int length,           // Length of data in message
-                                      BYTE *message,        // Bytes to perform operation on
-                                      BYTE *out,            // Buffer into which operation output is written
-                                      unsigned int *size);  // Size of returned data written here
+kssl_error_code private_key_operation(
+  pk_list     list,     // Private key array from new_pk_list
+  int         key_id,   // ID of key in pk_list from find_private_key
+  int         opcode,   // Opcode from a KSSL message indicating the operation
+  int         length,   // Length of data in message
+  BYTE       *message,  // Bytes to perform operation on
+  BYTE       *out,      // Buffer into which operation output is written
+  unsigned int *size);  // Size of returned data written here
 
 // key_size: returns the size of an RSA key in bytes
-int key_size(pk_list list,  // Array of private keys from new_pk_list
-             int key_id);   // ID of key from find_private_key
+int key_size(
+  pk_list     list,     // Array of private keys from new_pk_list
+  int         key_id);  // ID of key from find_private_key
 
 #endif // INCLUDED_KSSL_PRIVATE_KEY
