c298c1c698005af9a5c2758358ebe1d61ee66b1b
Martin J. Levy <mahtin@users.noreply.github.com>
2014-09-19 13:26:41 -0700

    Added documentation to code about -6 flag
    Added documentation to code about -6 flag and added a gracious empty line to clearly show the -6 option code vs the existing code.

--- free-port.pl
+++ free-port.pl
@@ -15,12 +15,14 @@ use Getopt::Std;
 # This will create a socket listening for TCP connections on an
 # available port number. By not passing the LocalPort option (or by
 # passing in 0) the system chooses the port.
+# script takes one optional argument: -6 which performs the test using IPv6 space (vs. IPv4)
 
 my %options=();
 getopts("6", \%options);
-my $port = -1;
 my $use_ipv6 = 0;
 $use_ipv6 = 1 if ( $options{6} );
+
+my $port = -1;
 my $socket;
 if ( $use_ipv6 > 0 ) {
         $socket = IO::Socket::INET6->new(Listen => 1, Proto => 'tcp');
